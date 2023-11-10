e5edb8be20b75728f16ad86aff8bccfe66e7a70d
Alejandro <alejandrojrosas2001@gmail.com>
2023-11-10 13:41:44 -0400

    missing dependencies

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
-import express, { Application } from 'express';
+import express, { Application } from 'express'
 
-const app:Application = express();
+const app: Application = express()
 
-app.get('/ping',(req,res)=>{
-    res.send('todo piola');
+app.get('/ping', (_req, res) => {
+  res.send('todo piola')
 })
 
-const server = app.listen(3000, () =>{
-    console.log(`server on port ${3000}`);
-});
+app.listen(3000, () => {
+  console.log(`server on port ${3000}`)
+})
